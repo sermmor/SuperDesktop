ce4cb08d321ad252d42f14a696bc364c6bcff532
Sergio Martín <sermmor@outlook.com>
2020-08-04 13:50:25 +0200

    Autoscale (fill and maximize) background.

---
+++ Assets/scripts/AutoScaleBackgroundToCamera.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AutoScaleMode { MAXIMIZE, FULL};
+
+public class AutoScaleBackgroundToCamera : MonoBehaviour
+{
+    public AutoScaleMode mode;
+
+    void Start()
+    {
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+        
+        transform.localScale = new Vector3(1, 1, transform.localScale.z);
+        Vector2 newScale = getScaleForSpriteAutoScale(sr);
+        putMode(newScale);
+    }
+
+    Vector2 getScaleForSpriteAutoScale(SpriteRenderer sr) {
+        float width = sr.sprite.bounds.size.x;
+        float height = sr.sprite.bounds.size.y;
+        
+        float worldScreenHeight = Camera.main.orthographicSize * 2.0f;
+        float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+        return new Vector2(worldScreenWidth / width, worldScreenHeight / height);
+    }
+
+    void putMode(Vector2 newScale) {
+        switch(mode) {
+            case AutoScaleMode.MAXIMIZE:
+                maximize(newScale);
+            break;
+            case AutoScaleMode.FULL:
+                full(newScale);
+            break;
+        }
+    }
+
+    void maximize(Vector2 newScale) {
+        transform.localScale = newScale;
+    }
+
+    void full(Vector2 newScale) {
+        float maxScale = Mathf.Max(newScale.x, newScale.y);
+        transform.localScale = new Vector3(maxScale, maxScale, transform.localScale.z);
+    }
+}
---
+++ Assets/scripts/ScreenSize.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ScreenSize
+{
+    public static float GetScreenToWorldHeight
+    {
+        get
+        {
+            Vector2 topRightCorner = new Vector2(1, 1);
+            Vector2 edgeVector = Camera.main.ViewportToWorldPoint(topRightCorner);
+            var height = edgeVector.y * 2;
+            return height;
+        }
+    }
+    public static float GetScreenToWorldWidth
+    {
+        get
+        {
+            Vector2 topRightCorner = new Vector2(1, 1);
+            Vector2 edgeVector = Camera.main.ViewportToWorldPoint(topRightCorner);
+            Debug.Log(edgeVector.x);
+            var width = edgeVector.x * 2;
+            return width;
+        }
+    }
+}
