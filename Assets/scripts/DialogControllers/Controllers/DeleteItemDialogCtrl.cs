510ccf4e259062b5fd95d2603cf8caccc6be30b6
Sergio Martín <sermmor@outlook.com>
2020-08-06 17:53:11 +0200

    Implemented delete item context menu functionality.

---
+++ Assets/scripts/DialogControllers/Controllers/DeleteItemDialogCtrl.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeleteItemDialogCtrl: DialogController
+{
+    InputField nameFile;
+    
+    ContextualMenuManager contextualMenuManager;
+
+    protected override void doOnEnable()
+    {
+        if (isLaunchedForFistTime)
+        {
+            contextualMenuManager = DesktopRootReferenceManager.getInstance().contextualMenuManager;
+        }
+    }
+
+    protected override void doAceptDialog()
+    {
+        GameObject.Destroy(whoIsCallMe.DesktopItemCaller.gameObject);
+    }
+
+    protected override void clearFieldsDialog() { }
+}
